6b2a1663a7eab081ef363c4a99f7e1ad25c56552
Abdulateef Al-radaee <abdulateef.radaee@gmail.com>
2020-08-22 05:18:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your Circle class here
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+    this._pi = Math.PI;
+  }
+  get diameter() {
+    return this.radius * 2;
+  }
+  get circumference() {
+    return this._pi * this.radius * 2;
+  }
+  get area() {
+    console.log(this._pi);
+    return this._pi * Math.pow(this.radius, 2);
+  }
+
+  set diameter(diameter) {
+    this.radius = diameter / 2;
+  }
+
+  set circumference(circumference) {
+    this.radius = circumference / (this._pi * 2);
+  }
+
+  set area(area) {
+    this.radius = Math.sqrt(area / this._pi);
+  }
+}
+
+let circle = new Circle(6);
+circle.diameter = 38;
+console.log(circle.radius);
